f73d7a2213c7ab07036ea38a579aead12203ab91
yangtianyu <535173883@qq.com>
2024-01-04 01:42:39 +0800

    1

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -5,12 +5,13 @@
 <script setup>
 import { onMounted, ref } from "vue";
 import * as THREE from "three";
+
 const canvasDom = ref(null);
 const scene = new THREE.Scene();
 //创建相机
 const camera = new THREE.PerspectiveCamera(
   75,
-  window.innerWidth / window.innerHeight
+  window.innerWidth / window.innerWidth
 );
 //设置相机位置
 camera.position.set(0, 0, 10);
@@ -21,33 +22,26 @@ onMounted(() => {
   initRender();
 });
 const initRender = () => {
-  //初始化渲染器
+  // //初始化渲染器
   const renderer = new THREE.WebGL1Renderer({
     canvas: canvasDom.value,
     antialias: true,
   });
-  //设置渲染的尺寸大小
+  // //设置渲染的尺寸大小
   renderer.setSize(window.innerWidth, window.innerHeight);
-  //将webgl渲染的canvas内容添加到body上
+  // //将webgl渲染的canvas内容添加到body上
+  // renderer.render(scene, camera);
+
   renderer.render(scene, camera);
-  // window.addEventListener("resize", resize);
 };
 const createBox = () => {
   //添加物体
   //创建几何体
   const cube = new THREE.BoxGeometry(1, 1, 1);
-  const mat = new THREE.MeshBasicMaterial();
+  const mat = new THREE.MeshBasicMaterial({ color: 0xffff00 });
   const mesh = new THREE.Mesh(cube, mat);
   scene.add(mesh);
-  console.log(scene);
 };
-
-function update() {
-  base.update();
-}
-function resize() {
-  base.resize();
-}
 </script>
 
 <style scoped>
